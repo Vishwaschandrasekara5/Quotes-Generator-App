57d20632479a04b3798bb9c0ba47c75403052bda
Vishwa Sandun Chandrasekara <148675868+Vishwaschandrasekara5@users.noreply.github.com>
2025-02-28 12:52:46 +0530

    update dart files
    2025/02/28

--- lib/quote.dart
+++ lib/quote.dart
@@ -13,76 +13,97 @@ class _QuoteScreenState extends State<QuoteScreen> {
       author: 'Oscar Wilde',
       details: 'Oscar Wilde was an Irish poet and playwright.',
       category: 'Motivational',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVkQH8jp7MrO9GlanMjVzXUrexSVJqYC8K9Q&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVkQH8jp7MrO9GlanMjVzXUrexSVJqYC8K9Q&s',
     ),
     Quote(
       text: 'Do what you can, with what you have, where you are.',
       author: 'Theodore Roosevelt',
-      details: 'Theodore Roosevelt was the 26th President of the United States.',
+      details:
+          'Theodore Roosevelt was the 26th President of the United States.',
       category: 'Motivational',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSluHBVZPS2-zwruzr_Jvte5iHHJGMYH2a9Sw&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSluHBVZPS2-zwruzr_Jvte5iHHJGMYH2a9Sw&s',
     ),
     Quote(
       text: 'In the middle of difficulty lies opportunity.',
       author: 'Albert Einstein',
-      details: 'Einstein was a theoretical physicist who developed the theory of relativity.',
+      details:
+          'Einstein was a theoretical physicist who developed the theory of relativity.',
       category: 'Inspirational',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbwB8-VBOacbJPwYTG7xflxLZKdhwW319DYQ&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbwB8-VBOacbJPwYTG7xflxLZKdhwW319DYQ&s',
     ),
     Quote(
       text: 'It does not matter how slowly you go as long as you do not stop.',
       author: 'Confucius',
       details: 'Confucius was a Chinese philosopher and politician.',
       category: 'Inspirational',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq2eODqB-GBtIbpUVMk8JJ7ZY_22bLQqqZ2w&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq2eODqB-GBtIbpUVMk8JJ7ZY_22bLQqqZ2w&s',
     ),
     Quote(
       text: 'The best way to predict the future is to create it.',
       author: 'Peter Drucker',
       details: 'Peter Drucker was an Austrian-American management consultant.',
       category: 'Success',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiTF2Sg1t6V_YzUplvzlMbZtuR_JhN5HeJGQ&s',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiTF2Sg1t6V_YzUplvzlMbZtuR_JhN5HeJGQ&s',
     ),
     Quote(
       text: 'Opportunities don’t happen, you create them.',
       author: 'Chris Grosser',
       details: 'Chris Grosser is a business strategist.',
       category: 'Success',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVxq1v-g2tQT6mhoMc3HTHTgx9C1j4fmZ4xw&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVxq1v-g2tQT6mhoMc3HTHTgx9C1j4fmZ4xw&s',
     ),
     Quote(
       text: 'Happiness depends upon ourselves.',
       author: 'Aristotle',
       details: 'Aristotle was a Greek philosopher and polymath.',
       category: 'Happiness',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrwBg2C8IagKcnyG47fq4autjt3jy2mT94Ug&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrwBg2C8IagKcnyG47fq4autjt3jy2mT94Ug&s',
     ),
     Quote(
-      text: 'Happiness is not something ready-made. It comes from your own actions.',
+      text:
+          'Happiness is not something ready-made. It comes from your own actions.',
       author: 'Dalai Lama',
       details: 'Dalai Lama is a spiritual leader of Tibet.',
       category: 'Happiness',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpf_-IEj4dlMtUzAWZtNsXJEq4Zb66-iqocg&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpf_-IEj4dlMtUzAWZtNsXJEq4Zb66-iqocg&s',
     ),
     Quote(
       text: 'A healthy outside starts from the inside.',
       author: 'Robert Urich',
       details: 'Robert Urich was an American film actor.',
       category: 'Health',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaYZ_v7DZM0sfer3CGJOLymwhG-fbDhxgdhA&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaYZ_v7DZM0sfer3CGJOLymwhG-fbDhxgdhA&s',
     ),
     Quote(
       text: 'Take care of your body. It’s the only place you have to live.',
       author: 'Jim Rohn',
-      details: 'Jim Rohn was an American entrepreneur and motivational speaker.',
+      details:
+          'Jim Rohn was an American entrepreneur and motivational speaker.',
       category: 'Health',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyakwmvR_eqbneOkMGsBfTM5zUubUArRhP4Q&s', 
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyakwmvR_eqbneOkMGsBfTM5zUubUArRhP4Q&s',
     ),
   ];
 
   late Quote currentQuote;
   String selectedCategory = "All";
-  final List<String> categories = ["All", "Motivational", "Inspirational", "Success", "Happiness", "Health"];
+  final List<String> categories = [
+    "All",
+    "Motivational",
+    "Inspirational",
+    "Success",
+    "Happiness",
+    "Health"
+  ];
 
   @override
   void initState() {
@@ -139,7 +160,8 @@ class _QuoteScreenState extends State<QuoteScreen> {
             items: categories.map((String category) {
               return DropdownMenuItem<String>(
                 value: category,
-                child: Text(category, style: const TextStyle(color: Colors.orange)),
+                child: Text(category,
+                    style: const TextStyle(color: Colors.orange)),
               );
             }).toList(),
           ),
@@ -159,12 +181,12 @@ class _QuoteScreenState extends State<QuoteScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 GestureDetector(
-                  onTap: () => navigateToQuoteDetails(currentQuote), 
+                  onTap: () => navigateToQuoteDetails(currentQuote),
                   child: Container(
                     margin: const EdgeInsets.only(top: 120),
                     child: Card(
                       elevation: 4,
-                      color: Colors.white.withOpacity(0.2),
+                      color: const Color.fromARGB(255, 31, 29, 29).withOpacity(0.5),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
@@ -179,7 +201,8 @@ class _QuoteScreenState extends State<QuoteScreen> {
                               Text(
                                 '"${currentQuote.text}"',
                                 style: const TextStyle(
-                                  fontSize: 20,
+                                  fontSize: 28, // Increased font size
+                                  fontWeight: FontWeight.bold, // Bold text
                                   fontStyle: FontStyle.italic,
                                   color: Color.fromARGB(255, 255, 255, 255),
                                 ),
@@ -204,7 +227,7 @@ class _QuoteScreenState extends State<QuoteScreen> {
                 ),
                 const SizedBox(height: 10),
                 SizedBox(
-                  width: double.infinity,
+                  width: 300,
                   height: 50,
                   child: ElevatedButton(
                     onPressed: fetchRandomQuote,
@@ -215,7 +238,8 @@ class _QuoteScreenState extends State<QuoteScreen> {
                         borderRadius: BorderRadius.circular(8),
                       ),
                     ),
-                    child: const Text('New Quote', style: TextStyle(fontSize: 18)),
+                    child:
+                        const Text('New Quote', style: TextStyle(fontSize: 18)),
                   ),
                 ),
               ],
--- lib/quote_details.dart
+++ lib/quote_details.dart
@@ -5,14 +5,14 @@ class Quote {
   String author;
   String details;
   String category;
-  String imageUrl; 
+  String imageUrl;
 
   Quote({
     required this.text,
     required this.author,
     required this.details,
     required this.category,
-    required this.imageUrl, 
+    required this.imageUrl,
   });
 }
 
@@ -27,43 +27,83 @@ class QuoteDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Quote Details'),
         backgroundColor: Colors.orange,
+        automaticallyImplyLeading: false, // Removes the default back button
       ),
-      body: Center( 
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, 
-            crossAxisAlignment: CrossAxisAlignment.center, 
-            children: [
-              
-              CircleAvatar(
-                radius: 80,
-                backgroundImage: NetworkImage(quote.imageUrl), 
-                onBackgroundImageError: (error, stackTrace) {
-                  
-                  
-                },
+      body: Container(
+        color: const Color.fromARGB(255, 31, 29, 29), // Background color set to black
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 80,
+                        backgroundImage: NetworkImage(quote.imageUrl),
+                        onBackgroundImageError: (error, stackTrace) {},
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        '"${quote.text}"',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white, // White text for readability
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Author: ${quote.author}',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Details: ${quote.details}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                '"${quote.text}"',
-                style: const TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
-                textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0), // Adds spacing at the bottom
+              child: SizedBox(
+                width: 200, // Fixed button width
+                height: 50, // Fixed button height
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, // Button color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8), // Rounded edges
+                    ),
+                  ),
+                  child: const Text(
+                    'Back',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Author: ${quote.author}',
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Details: ${quote.details}',
-                style: const TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
